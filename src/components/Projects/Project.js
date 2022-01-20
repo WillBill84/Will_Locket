0988f2ca9d3ecc6e51f2cfeee15104c6e5e97e2d
Will Locket <will.locket@fastmail.com>
2022-01-20 22:26:04 +1100

    project order

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -13,8 +13,8 @@ class Projects extends Component {
             <section id="projects" className="projects">
             <Container fluid="xxl" >
             <h1 className="bigHeader">Projects</h1>
+            <Project imgsrc={JSG} imgalt={"Jenny Smith Gardens"} title={"Jenny Smith Gardens"} description={"I did the UX/UI designs and was involved with the development of this custom responsive website while working at Left Brain Solutions."} link={"http://www.jsg.net.au/"} />
                 <Project imgsrc={Level5} imgalt={"Level 5 Projects website"} title={"Level 5 Projects"} description={"I worked with Left Brain Solutions to help create a custom website for Level 5 Projects. It is a responsive website that uses Joomla for the cms."} link={"https://level5projects.com.au"} />
-                <Project imgsrc={JSG} imgalt={"Jenny Smith Gardens"} title={"Jenny Smith Gardens"} description={"I did the UX/UI designs and was involved with the development of this custom responsive website while working at Left Brain Solutions."} link={"http://www.jsg.net.au/"} />
                 <Project imgsrc={BLsite} imgalt={"Ben Locket website"} title={"Ben Locket"} description={"I worked with Ben to customise his Wordpress website to display his portfolio of Film and T.V cues in a contemporary minimalist style."} link={"http://benlocket.com"} />
                 <Project imgsrc={PCR} imgalt={"Penguin Coolroom Hire"} title={"Penguin Coolroom Hire"} description={"UX/UI template redesign and mockups for Penguin Coolroom Hire's responsive website."} link={"https://xd.adobe.com/view/84523429-9084-4bef-86a0-50543cecf5e6-d101/screen/f9d1dad5-6533-4f3f-9d05-c0f37f8eb4ae?hints=off"} />
             </Container>
